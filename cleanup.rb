242fe869a5a822da62d642703071cf355faee5c0
Haru <harukwork@gmail.com>
2025-11-12 10:26:15 -0500

    lang pretty print + filepath local

--- cleanup.rb
+++ cleanup.rb
@@ -3,7 +3,9 @@ require 'fileutils'
 require 'hashie'
 
 $lang = {}
-File.open("D:/CommunityDragon/lang/lol.stringtable.json", 'rb') { |f| $lang = JSON.parse(f.read()).values[0] }
+File.open("lang/lol.stringtable.json", 'rb') { |f| $lang = JSON.parse(f.read()) }
+$lang = $lang["entries"] || $lang
+File.open("lang/lol.stringtable.json", 'wb') { |f| f.write(JSON.pretty_generate($lang)) }
 
 $maps = {
     11 => "Summoner's Rift",
@@ -25,8 +27,6 @@ def removeFluff(obj)
     }
 
 end
-
-home = "D:/CommunityDragon"
 
 filter = [
     "bloom_",
@@ -57,17 +57,17 @@ filter = [
 
 # Arena handling
 arena = {}
-File.open("#{home}/arena/en_us.json", 'rb') { |f| arena = JSON.parse(f.read()) }
-File.open("#{home}/arena/en_us.json", 'wb') { |f| f.write(JSON.pretty_generate(arena)) }
+File.open("arena/en_us.json", 'rb') { |f| arena = JSON.parse(f.read()) }
+File.open("arena/en_us.json", 'wb') { |f| f.write(JSON.pretty_generate(arena)) }
 
 # ARAM: Mayhem Augment handling
 aram = {}
-File.open("#{home}/mayhem/augments.bin.json", 'rb') { |f| aram = JSON.parse(f.read()) }
-File.open("#{home}/mayhem/augments.bin.json", 'wb') { |f| f.write(JSON.pretty_generate(aram)) }
+File.open("mayhem/augments.bin.json", 'rb') { |f| aram = JSON.parse(f.read()) }
+File.open("mayhem/augments.bin.json", 'wb') { |f| f.write(JSON.pretty_generate(aram)) }
 
 deletions = []
-Dir.each_child("#{home}/out") { |path|
-    basepath = home + "/out/" + path
+Dir.each_child("out") { |path|
+    basepath = "out/" + path
     if filter.any? { |prefix| path.start_with?(prefix) }
         deletions.push(basepath)
     else
@@ -85,17 +85,17 @@ Dir.each_child("#{home}/out") { |path|
 }
 
 items = {}
-File.open("#{home}/items/items.cdtb.bin.json", 'rb') { |f| items = JSON.parse(f.read()) }
+File.open("items/items.cdtb.bin.json", 'rb') { |f| items = JSON.parse(f.read()) }
 
 items.each { |item, itemObj|
     if itemObj.is_a?(Hash)
         itemObj.each { |k, v|
             next if !v.is_a?(String)
-            itemObj[k] = $lang.fetch(v.downcase, v) if k == "mDisplayName"
+            itemObj[k] = $lang.fetch(v.downcase, v) if v.include?("_") && k == "mDisplayName"
         }
     end
 }
 
-File.open("#{home}/items/items.cdtb.bin.json", 'wb') { |f| f.write(JSON.pretty_generate(items)) }
+File.open("items/items.cdtb.bin.json", 'wb') { |f| f.write(JSON.pretty_generate(items)) }
 
 FileUtils.rm_rf(deletions)
