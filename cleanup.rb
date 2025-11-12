133f39cf06254e03fe64e8b08da50734d911ee1d
Haru <harukwork@gmail.com>
2025-11-12 13:39:20 -0500

    misc game data

--- cleanup.rb
+++ cleanup.rb
@@ -2,27 +2,6 @@ require 'json'
 require 'fileutils'
 require 'hashie'
 
-$lang = {}
-File.open("lang/lol.stringtable.json", 'rb') { |f| $lang = JSON.parse(f.read()) }
-$lang = $lang["entries"] || $lang
-File.open("lang/lol.stringtable.json", 'wb') { |f| f.write(JSON.pretty_generate($lang)) }
-loadtips = {}
-$lang.each { |key, string|
-    next if !key.start_with?("game_startup_tip_")
-    id, category = key.split("game_startup_tip_")[1].split("_")
-    loadtips[category] ||= {}
-    loadtips[category].store(id, string)
-}
-Dir.mkdir("loadtips") unless Dir.exist?("loadtips")
-File.open("loadtips/loadtips.json", 'wb') { |f| f.write(JSON.pretty_generate(loadtips)) }
-
-$maps = {
-    11 => "Summoner's Rift",
-    12 => "ARAM",
-    21 => "Nexus Blitz",
-    30 => "Arena",
-}
-
 def removeFluff(obj)
     values = [
         "ItemRecommendationOverrideSet",
@@ -37,6 +16,46 @@ def removeFluff(obj)
 
 end
 
+print "Loading and formatting stringtable..."
+$lang = {}
+File.open("lang/lol.stringtable.json", 'rb') { |f| $lang = JSON.parse(f.read()) }
+$lang = $lang["entries"] || $lang
+File.open("lang/lol.stringtable.json", 'wb') { |f| f.write(JSON.pretty_generate($lang)) }
+print "done.\n"
+
+print "Extracting loading screen tips..."
+loadtips = {}
+$lang.each { |key, string|
+    next if !key.start_with?("game_startup_tip_")
+    id, category = key.split("game_startup_tip_")[1].split("_")
+    loadtips[category] ||= {}
+    loadtips[category].store(id, string.gsub("''", "'"))
+}
+Dir.mkdir("loadtips") unless Dir.exist?("loadtips")
+File.open("loadtips/loadtips.json", 'wb') { |f| f.write(JSON.pretty_generate(loadtips)) }
+print "done.\n"
+
+print "Loading and formatting map data..."
+$maps = {}
+File.open("game-data/maps.json", 'rb') { |f| $maps = JSON.parse(f.read()) }
+File.open("game-data/maps.json", 'wb') { |f| f.write(JSON.pretty_generate($maps)) }
+print "done.\n"
+
+# Arena handling
+print "Loading and formatting Arena augment data..."
+arena = {}
+File.open("arena/en_us.json", 'rb') { |f| arena = JSON.parse(f.read()) }
+File.open("arena/en_us.json", 'wb') { |f| f.write(JSON.pretty_generate(arena)) }
+print "done.\n"
+
+# ARAM: Mayhem Augment handling
+print "Loading and formatting ARAM: Mayhem augment data..."
+aram = {}
+File.open("mayhem/augments.bin.json", 'rb') { |f| aram = JSON.parse(f.read()) }
+File.open("mayhem/augments.bin.json", 'wb') { |f| f.write(JSON.pretty_generate(aram)) }
+print "done.\n"
+
+print "Loading and formatting champion data..."
 filter = [
     "bloom_",
     "brawl_",
@@ -62,18 +81,6 @@ filter = [
     "ultbook",
     "urf_"
 ]
-
-
-# Arena handling
-arena = {}
-File.open("arena/en_us.json", 'rb') { |f| arena = JSON.parse(f.read()) }
-File.open("arena/en_us.json", 'wb') { |f| f.write(JSON.pretty_generate(arena)) }
-
-# ARAM: Mayhem Augment handling
-aram = {}
-File.open("mayhem/augments.bin.json", 'rb') { |f| aram = JSON.parse(f.read()) }
-File.open("mayhem/augments.bin.json", 'wb') { |f| f.write(JSON.pretty_generate(aram)) }
-
 deletions = []
 Dir.each_child("out") { |path|
     basepath = "out/" + path
@@ -86,13 +93,19 @@ Dir.each_child("out") { |path|
                 deletions.push(filepath)
             else
                 champ = {}
-                File.open(filepath, 'rb') { |f| champ = JSON.parse(f.read()) }                
+                File.open(filepath, 'rb') { |f| champ = JSON.parse(f.read()) }
+
                 File.open(filepath, 'wb') { |f| f.write(JSON.pretty_generate(removeFluff(champ))) }
             end
         }
     end
 }
+print "done.\n"
+print "Deleting bloat (see 'filter' variable in code for modifying)..."
+FileUtils.rm_rf(deletions)
+print "done.\n"
 
+print "Loading and formatting item data..."
 items = {}
 File.open("items/items.cdtb.bin.json", 'rb') { |f| items = JSON.parse(f.read()) }
 
@@ -106,5 +119,33 @@ items.each { |item, itemObj|
 }
 
 File.open("items/items.cdtb.bin.json", 'wb') { |f| f.write(JSON.pretty_generate(items)) }
+print "done.\n"
 
-FileUtils.rm_rf(deletions)
+print "Loading and formatting miscellaneous game data..."
+Dir.each_child("game-data") { |path|
+    data = {}
+    File.open("game-data/#{path}", 'rb') { |f| data = JSON.parse(f.read()) }
+
+    File.open("game-data/#{path}", 'wb') { |f| f.write(JSON.pretty_generate(data)) }
+}
+
+queues = {}
+File.open("game-data/queues.json", 'rb') { |f| queues = JSON.parse(f.read()) }
+champs = {}
+File.open("game-data/champion-summary.json", 'rb') { |f| 
+    c = JSON.parse(f.read()) 
+    c.each { |champ|
+        next if !champ.is_a?(Hash)
+        id = champ["id"]
+        name = champ["name"]
+        champs.store(id, name)
+    }
+}
+
+queues.each { |queue|
+    next if !queue.is_a?(Hash)
+    next if !queue["viableChampionRoster"]
+    queue["viableChampionRoster"] = queue["viableChampionRoster"].map { |v| champs.fetch(v, v) }
+}
+File.open("game-data/queues.json", 'wb') { |f| f.write(JSON.pretty_generate(queues)) }
+print "done.\n"
--- latest.bat
+++ latest.bat
@@ -7,5 +7,7 @@ snip-snip https://raw.communitydragon.org/latest/game/data/characters/ --max-dep
 snip-snip https://raw.communitydragon.org/latest/game/ --filter "items.cdtb.bin.json" -o "items" --max-depth 1
 
 snip-snip https://raw.communitydragon.org/latest/game/maps/modespecificdata/ --filter "augments.bin.json" -o "mayhem" --max-depth 1
+snip-snip https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/ -o "game-data" --max-depth 1
+
 ruby cleanup.rb
 pause
